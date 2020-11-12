4b980d91c5c91b324ca0f40f4d2b6719997a772b
standridgedevelopment <standridgedevelopment@gmail.com>
2020-11-12 11:25:13 -0500

    Add project files.

---
+++ LocalSpirits.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalSpirits.WebMVC.MvcApplication" Language="C#" %>
---
+++ LocalSpirits.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalSpirits.WebMVC.Startup))]
+namespace LocalSpirits.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
