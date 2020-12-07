632411a07fd4aac08b40cb8a81752b9473e9442a
standridgedevelopment <standridgedevelopment@gmail.com>
2020-12-06 19:15:42 -0500

    Reply Scaffolding

---
+++ LocalSpirits.Models/Comment/ReplyCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalSpirits.Models.Comment
+{
+    public class Reply
+    {
+        public int? CommentID { get; set; }
+        [DisplayName("Reply")]
+        public string ReplyContent { get; set; }
+        public string SenderFullName { get; set; }
+        public string SenderUsername { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+    }
+}
---
+++ LocalSpirits.Models/Comment/ReplyListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalSpirits.Models.Comment
+{
+    public class ReplyListItem
+    {
+        public int ReplyID { get; set; }
+        public int? CommentID { get; set; }
+        public string SenderFullName { get; set; }
+        public string SenderUsername { get; set; }
+        public string ReplyContent { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Modified { get; set; }
+    }
+}
